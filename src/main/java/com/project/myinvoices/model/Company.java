40ef3ca2bab9f8357a7445066356470b708bde8d
Rahul Paryani <rparyani@asite.com>
2021-10-13 13:19:33 +0530

    Changes

--- src/main/java/com/project/myinvoices/model/Company.java
+++ src/main/java/com/project/myinvoices/model/Company.java
@@ -1,14 +1,9 @@
 package com.project.myinvoices.model;
 
-import java.util.List;
-
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
-
 import lombok.Getter;
 import lombok.Setter;
 
